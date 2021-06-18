436d579b3c84baf433e185414edf9c6b23e596be
Masozera <masozeravictor@gmail.com>
2021-06-18 03:09:59 -0700

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+$(function(){
+    // $('#about').click(function() {
+    //     $(this).slideDown(5000);
+
+    // });
+    $('.about_heading').css('color','red');
+
+    // Back to top button
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+
+  $('.back-to-top').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+  });
+
+})(jQuery);
